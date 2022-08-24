572cad6ed3d4bb35d18129ae6894fcde0379daab
Facundo Rodriguez <facundo-grodriguez@hotmail.com.ar>
2022-08-24 15:33:34 -0300

    calculador de coutas y descuento

---
+++ entregable.js
@@ -0,0 +1,39 @@
+
+
+let monto = parseFloat(prompt("ingrese el monto del producto"))
+
+
+let cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas 1, 3, 6, 9, 12 , 18"))
+
+
+let promo =((monto * 10) / 100)
+
+
+let montoConCuota = monto
+
+
+
+
+const calculador = () => {
+
+    if (cantidadCuotas != 1) {
+        for (i = 1; i <= cantidadCuotas; i++) {
+            let porCuota =((monto * 5) / 100)
+            montoConCuota = montoConCuota + porCuota
+
+        }
+        let valorPorCuota = (montoConCuota / cantidadCuotas).toFixed(2)
+
+        document.write( "El valor del producto es $" + monto + " la cantidad de cuotas son " + cantidadCuotas + " y el precio por cada una es de $" + valorPorCuota + " lo cual queda en un monto de $" + montoConCuota)
+
+    } else {
+        document.write( "El valor del producto es $" + monto + " y estas dentro del 10% de descuento por un pago y queda en $" + (monto - promo))
+
+    }
+
+}
+
+calculador()
+
+
+
